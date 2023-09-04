40167c5ff6f055602df733ee41942480694ecf43
woXrooX <woXrooX@gmail.com>
2023-09-04 20:30:24 +0500

    photo tables ready

--- Database/db.sql
+++ Database/db.sql
@@ -195,3 +195,34 @@ CREATE TABLE IF NOT EXISTS `users_roles` (
 
   PRIMARY KEY (`id`)
 );
+
+
+
+-- -------------------------- Surgery Tashkent
+CREATE TABLE IF NOT EXISTS `photos` (
+  `id` INT NOT NULL UNIQUE auto_increment,
+  `name` INT NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `gallery` (
+  `id` INT NOT NULL UNIQUE auto_increment,
+  `photo` INT NOT NULL,
+
+  FOREIGN KEY (`photo`) REFERENCES photos(`id`) ON DELETE CASCADE,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` INT NOT NULL UNIQUE auto_increment,
+  `photo` INT NOT NULL,
+
+  FOREIGN KEY (`photo`) REFERENCES photos(`id`) ON DELETE CASCADE,
+
+  PRIMARY KEY (`id`)
+);
+
+
+
