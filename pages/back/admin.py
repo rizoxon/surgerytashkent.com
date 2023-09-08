e98f3e2d03b25cc3394a9a8daeec415d36c7578a
woXrooX <woXrooX@gmail.com>
2023-09-08 17:58:54 +0500

    comment get/delete

--- pages/back/admin.py
+++ pages/back/admin.py
@@ -10,4 +10,27 @@ def admin():
     if request.method == "GET": return render_template("index.html", **globals())
 
     elif request.method == "POST":
-        pass
+        ############################## FORM
+        if "multipart/form-data" in request.content_type.split(';'):
+            if request.form["for"] == "deleteComment":
+                if "id" not in request.form or not request.form['id']:
+                    return response(type="error", message="databaseError", toast=True)
+
+                data = MySQL.execute(
+                    sql="DELETE FROM comments WHERE id = %s",
+                    params=(request.form['id'],),
+                    commit=True
+                )
+
+                if data is False: return response(type="error", message="databaseError", toast=True)
+
+                return response(type="success",message="deleted",toast=True, domChange=["main"])
+
+        ############################## JSON
+        if request.content_type == "application/json":
+            if request.get_json()["for"] == "getAllComments":
+                data = MySQL.execute("SELECT * FROM comments")
+
+                if data is False: return response(type="error",message="databaseError")
+
+                return response(type="success",message="success",data=data)
--- pages/front/admin.js
+++ pages/front/admin.js
@@ -1,7 +1,60 @@
 export const TITLE = window.Lang.use("admin");
 
+export async function before(){
+  window.pageData.resp = await window.bridge("admin", {for:"getAllComments"});
+
+  window.pageData.commentsRows = "No data";
+
+  if("data" in window.pageData.resp){
+    window.pageData.commentsRows = "";
+    for(const data of window.pageData.resp.data)
+    window.pageData.commentsRows += `
+      <tr>
+        <td>${data["fullName"]}</td>
+        <td>${data["phoneNumber"]}</td>
+        <td>${data["comment"]}</td>
+        <td>
+          <form action="/admin" for="deleteComment">
+            <input type="hidden" name="id" value="${data["id"]}">
+            <button name="deleteCommentButton"><x-icon name="trash"></x-icon></button>
+          </form>
+        </td>
+      </tr>
+    `;
+  }
+}
+
 export default function content(){
+  return `
+    <container>
+      <row class="gap-1 p-5">
+        <column id="sections" class="w-auto">
+          <x-nav selector="column#sections">
+            [
+              {"link": "commnets"}
+            ]
+          </x-nav>
+        </column>
 
-  return ``;
+        <column id="sections" class="w-70">
+          <section id="commnets" class="surface-clean w-100 p-1">
+            <table class="x-skeleton scrollbar-x">
 
+              <thead>
+                <tr>
+                  <th>Full name</th>
+                  <th>Phone number</th>
+                  <th>Comment</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+
+              <tbody>${window.pageData.commentsRows}<tbody>
+            </table>
+
+          </section>
+        </column>
+      </row>
+    </container>
+  `;
 }
