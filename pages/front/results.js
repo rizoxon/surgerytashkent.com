cd1d4b4b6ed632b669b789aa00273ef811df28bf
rizoxon <madaminov.co@gmail.com>
2023-10-18 20:21:48 +0500

    results v2

--- pages/front/results.js
+++ pages/front/results.js
@@ -1,14 +1,14 @@
 export default function content(){
   return `
     <container class="results">
-      <row class="w-80">
+      <row>
         <button><a href="#operations">Operatsiyalar</a></button>
         <button><a href="#results">Avval va Keyingi rasmlar</a></button>
         <button><a href="#others">Boshqa rasmlar</a></button>
       </row>
 
-      <h2 class="surface-2D w-100 text-center bg-info" style="color:white;">Operatsiyalar</h2>
-      <row class="gap-1 p-3" id="operations">
+      <h2  id="operations" class="surface-2D w-100 text-center bg-info" style="color:white;">Operatsiyalar</h2>
+      <row class="gap-1 p-3">
         <column class="p-1 surface-2D gap-1">
           <img src="images/general_surgery.jpg" class="w-100">
           <p>Lorem ipsum dolor sit amet.</p>
@@ -42,8 +42,78 @@ export default function content(){
           <p>Lorem ipsum dolor sit amet.</p>
         </column>
       </row>
-      <row id="results"></row>
-      <row id="others"></row>
+
+      <h2 id="results" class="surface-2D w-100 text-center bg-info" style="color:white;">Avval va Keyingi rasmlar</h2>
+      <row>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+      </row>
+
+      <h2 id="others" class="surface-2D w-100 text-center bg-info" style="color:white;">Boshqa rasmlar</h2>
+      <row>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+        <column class="p-1 surface-2D gap-1">
+          <img src="images/general_surgery.jpg" class="w-100">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </column>
+      </row>
     </container>
   `;
 }
