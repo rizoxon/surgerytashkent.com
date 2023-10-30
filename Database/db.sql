3d8c34e6a9db9e93392c17c839be488b4eddd1e0
rizoxon <madaminov.co@gmail.com>
2023-10-30 22:03:05 +0500

    db name

--- Database/db.sql
+++ Database/db.sql
@@ -2,9 +2,9 @@
 \! echo "--------------- X ---------------";
 \W
 
-DROP DATABASE IF EXISTS surgery_tashkent;
-CREATE DATABASE IF NOT EXISTS surgery_tashkent CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE surgery_tashkent;
+DROP DATABASE IF EXISTS surgerytashkent_db;
+CREATE DATABASE IF NOT EXISTS surgerytashkent_db CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE surgerytashkent_db;
 
 SET NAMES utf8mb4;
 
